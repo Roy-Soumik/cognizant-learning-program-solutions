3b183b9181701988e794e3abf5df6d3010821b9c
Roy-Soumik <124526184+Roy-Soumik@users.noreply.github.com>
2025-06-29 23:58:39 +0530

    Added Week 2

---
+++ Week 2/JUnit, Mockito and SL4J/my-junit-project/src/main/java/com/my_junit_project/MyJunitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.my_junit_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJunitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJunitProjectApplication.class, args);
+	}
+
+}
