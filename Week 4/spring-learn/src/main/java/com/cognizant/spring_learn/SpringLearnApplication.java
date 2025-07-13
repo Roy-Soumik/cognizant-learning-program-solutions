29e42c72218b48f6b68b79ab183f796d8aa65e23
Roy-Soumik <mesoumik095@gmail.com>
2025-07-13 19:48:05 +0530

    Week 4 - added Hands on 2 of 1.spring-rest-handson

--- Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -4,15 +4,37 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 @SpringBootApplication
 public class SpringLearnApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
+
+	public static void displayDate() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
+			Date date = format.parse("13/07/2025");
+			System.out.println(date);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		LOGGER.info("Starting Application");
 		SpringApplication.run(SpringLearnApplication.class, args);
 		LOGGER.info("Exiting Application");
+
+		displayDate();
 	}
 
 }
