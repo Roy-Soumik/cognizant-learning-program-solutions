1b3bf462efa7011fea8b3c7bc447717839d943bb
Roy-Soumik <mesoumik095@gmail.com>
2025-07-13 21:44:43 +0530

    Week 4 - added Hands on 3 of 2.spring-rest-handson

--- Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -1,10 +1,13 @@
 package com.cognizant.spring_learn.controller;
 
 import com.cognizant.spring_learn.Country;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 @RestController
 public class CountryController {
@@ -13,5 +16,11 @@ public class CountryController {
     public Country getCountryIndia() {
         ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
         return context.getBean("country", Country.class);
+    }
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        return context.getBean("countryList", List.class);
     }
 }
