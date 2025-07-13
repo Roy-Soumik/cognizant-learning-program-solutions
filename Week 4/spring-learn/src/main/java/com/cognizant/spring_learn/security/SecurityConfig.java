612624ba514813139eed74f22176a9947838ebad
Roy-Soumik <mesoumik095@gmail.com>
2025-07-13 23:39:30 +0530

    Week 4 - added Hands on 1 of 5.spring-rest-handson

---
+++ Week 4/spring-learn/src/main/java/com/cognizant/spring_learn/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.cognizant.spring_learn.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER");
+    }
+}
