20122a0315ad5072877e9d8d830fc595d4851ce1
Roy-Soumik <124526184+Roy-Soumik@users.noreply.github.com>
2025-07-27 16:00:42 +0530

    Week 6 - Added Hands On 1-4

---
+++ Week 6/4. ReactJS-HOL/blogapp/src/Posts.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => {
+        const postList = data.map(item => new Post(item.id, item.title, item.body));
+        this.setState({ posts: postList });
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred: " + error);
+  }
+}
+
+export default Posts;
